cef597dcb94ceee69df963e2b6f74b3f97da3272
aerosouund <aerosound161@gmail.com>
2024-05-11 18:10:40 +0300

    ssh func

--- cluster-provision/gocli/cmd/ssh.go
+++ cluster-provision/gocli/cmd/ssh.go
@@ -1,10 +1,14 @@
 package cmd
 
 import (
+	"bytes"
+	"fmt"
+	"log"
 	"os"
 
 	"github.com/docker/docker/client"
 	"github.com/spf13/cobra"
+	ssh1 "golang.org/x/crypto/ssh"
 	"golang.org/x/crypto/ssh/terminal"
 	"kubevirt.io/kubevirtci/cluster-provision/gocli/docker"
 )
@@ -55,6 +59,61 @@ func ssh(cmd *cobra.Command, args []string) error {
 			exitCode = 1
 		}
 		os.Exit(exitCode)
+	}
+	return nil
+}
+
+func hostSSH(nodeIdx int, dnsmasqID string, sshPort int16, cmd string) error {
+	// docker exec socat on the dnsmasq
+	success, err := docker.Exec(cli, dnsmasqID, []string{
+		"socat",
+		"TCP-LISTEN:2222,fork,reuseaddr",
+		fmt.Sprintf("TCP:192.168.66.1%s:22", nodeIdx),
+	}, os.NewFile(0, os.DevNull))
+	if err != nil {
+		return err
+	}
+	if !success {
+		return fmt.Errorf("failed to execute cmd %s on node %+v", cmd, nodeIdx)
+	}
+
+	defer func() {
+		docker.Exec(cli, dnsmasqID, []string{
+			"sh", "-c", "pkill socat",
+		}, os.NewFile(0, os.DevNull))
+	}()
+
+	signer, err := ssh1.ParsePrivateKey([]byte(sshKey))
+	if err != nil {
+		return err
+	}
+
+	config := &ssh1.ClientConfig{
+		User: "vagrant",
+		Auth: []ssh1.AuthMethod{
+			ssh1.PublicKeys(signer),
+		},
+		HostKeyCallback: ssh1.InsecureIgnoreHostKey(),
+	}
+
+	client, err := ssh1.Dial("tcp", "localhost:"+string(sshPort), config)
+	if err != nil {
+		return fmt.Errorf("Failed to connect to SSH server: %v", err)
+	}
+	defer client.Close()
+
+	session, err := client.NewSession()
+	if err != nil {
+		log.Fatalf("Failed to create SSH session: %v", err)
+	}
+	defer session.Close()
+
+	var b bytes.Buffer
+	session.Stderr = &b
+
+	err = session.Run(cmd)
+	if err != nil {
+		return fmt.Errorf("Failed to execute command: %v", err)
 	}
 	return nil
 }
