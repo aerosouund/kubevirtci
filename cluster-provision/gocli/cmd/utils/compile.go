b62c7e4ee430a437338a6e49a8724fec13726138
aerosouund <aerosound161@gmail.com>
2024-05-13 21:11:37 +0300

    golang imahe

--- cluster-provision/gocli/cmd/utils/compile.go
+++ cluster-provision/gocli/cmd/utils/compile.go
@@ -9,7 +9,7 @@ import (
 // this will be used to compile go code to a target os then scp it to the vm to be executed
 func CompileToTargetOS(location string) error {
 	cmd := exec.Command(
-		"go", "build", "-o", fmt.Sprintf("./bin/%s", location), fmt.Sprintf("./scripts/%s", location),
+		"/usr/bin/go", "build", "-o", fmt.Sprintf("./bin/%s", location), fmt.Sprintf("./scripts/%s", location),
 	)
 	var stdout bytes.Buffer
 	var stderr bytes.Buffer
