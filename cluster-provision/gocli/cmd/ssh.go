82f1693cf9095d466659dec8a65eba421851bdbf
aerosouund <aerosound161@gmail.com>
2024-05-13 20:10:17 +0300

    delete socat implementation

--- cluster-provision/gocli/cmd/ssh.go
+++ cluster-provision/gocli/cmd/ssh.go
@@ -6,7 +6,6 @@ import (
 	"log"
 	"net"
 	"os"
-	"time"
 
 	"github.com/docker/docker/client"
 	"github.com/spf13/cobra"
@@ -106,78 +105,6 @@ func jumpSSH(nodeIdx int, sshPort uint16, cmd string) (string, error) {
 
 	session.Stderr = &stderr
 	session.Stdout = &stdout
-
-	err = session.Run(cmd)
-	if err != nil {
-		return "", fmt.Errorf("Failed to execute command: %v", err)
-	}
-	return stdout.String(), nil
-}
-
-func hostSSH(nodeIdx int, dnsmasqID string, sshPort uint16, cmd string) (string, error) {
-	success, err := docker.Exec(cli, dnsmasqID, []string{
-		"yum", "install", "socat", "-y",
-	}, os.Stdout)
-	if err != nil {
-		return "", err
-	}
-	if !success {
-		return "", fmt.Errorf("failed to install socat")
-	}
-	// docker exec socat on the dnsmasq
-	go func() {
-		success, err = docker.Exec(cli, dnsmasqID, []string{
-			"/usr/bin/socat",
-			"TCP-LISTEN:2222,fork,reuseaddr",
-			fmt.Sprintf("TCP:192.168.66.10%d:22", nodeIdx),
-		}, os.Stdout)
-		if err != nil {
-			// Handle error if needed
-			fmt.Printf("Error executing socat command: %v\n", err)
-		}
-		if !success {
-			// Handle failure if needed
-			fmt.Printf("Failed to execute socat command\n")
-		}
-	}()
-
-	defer func() {
-		docker.Exec(cli, dnsmasqID, []string{
-			"sh", "-c", "pkill socat",
-		}, os.NewFile(0, os.DevNull))
-	}()
-
-	signer, err := ssh1.ParsePrivateKey([]byte(sshKey))
-	if err != nil {
-		return "", err
-	}
-
-	config := &ssh1.ClientConfig{
-		User: "vagrant",
-		Auth: []ssh1.AuthMethod{
-			ssh1.PublicKeys(signer),
-		},
-		HostKeyCallback: ssh1.InsecureIgnoreHostKey(),
-	}
-
-	client, err := ssh1.Dial("tcp", net.JoinHostPort("127.0.0.1", fmt.Sprint(sshPort)), config)
-	if err != nil {
-		return "", fmt.Errorf("Failed to connect to SSH server: %v", err)
-	}
-	defer client.Close()
-
-	session, err := client.NewSession()
-	if err != nil {
-		log.Fatalf("Failed to create SSH session: %v", err)
-	}
-	defer session.Close()
-
-	var stderr bytes.Buffer
-	var stdout bytes.Buffer
-
-	session.Stderr = &stderr
-	session.Stdout = &stdout
-	time.Sleep(time.Second * 500)
 
 	err = session.Run(cmd)
 	if err != nil {
