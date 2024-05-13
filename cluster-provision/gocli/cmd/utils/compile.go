9b5049661fa8469b259384d4cc69da0e43db5227
aerosouund <aerosound161@gmail.com>
2024-05-13 21:03:02 +0300

    compile

--- cluster-provision/gocli/cmd/utils/compile.go
+++ cluster-provision/gocli/cmd/utils/compile.go
@@ -7,7 +7,7 @@ import (
 )
 
 // this will be used to compile go code to a target os then scp it to the vm to be executed
-func compileToTargetOS(location string) error {
+func CompileToTargetOS(location string) error {
 	cmd := exec.Command("CGO_ENABLED=0",
 		"GOOS=linux", "GOARCH=amd64",
 		"go", "build", "-o", fmt.Sprintf("./bin/%s", location), fmt.Sprintf("./scripts/%s", location),
