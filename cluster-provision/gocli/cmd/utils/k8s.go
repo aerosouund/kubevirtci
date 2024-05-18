99827c1d3cab2b8c7dbaee580e56f9ba20b43a80
aerosouund <aerosound161@gmail.com>
2024-05-18 13:32:36 +0300

    k8s apply

--- cluster-provision/gocli/cmd/utils/k8s.go
+++ cluster-provision/gocli/cmd/utils/k8s.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"path/filepath"
 
 	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -14,75 +13,11 @@ import (
 	"k8s.io/apimachinery/pkg/runtime/serializer"
 	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/kubernetes/scheme"
-	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/rest"
 	"sigs.k8s.io/yaml"
 )
 
-type KubeConfig struct {
-	APIVersion string `yaml:"apiVersion"`
-	Clusters   []struct {
-		Cluster struct {
-			Server    string `yaml:"server"`
-			TLSVerify bool   `yaml:"insecure-skip-tls-verify"`
-		} `yaml:"cluster"`
-		Name string `yaml:"name"`
-	} `yaml:"clusters"`
-	Contexts []struct {
-		Context struct {
-			Cluster string `yaml:"cluster"`
-		} `yaml:"context"`
-		Name string `yaml:"name"`
-	} `yaml:"contexts"`
-	CurrentContext string   `yaml:"current-context"`
-	Kind           string   `yaml:"kind"`
-	Preferences    struct{} `yaml:"preferences"`
-	Users          []struct {
-		Name string `yaml:"name"`
-		User struct {
-			InsecureSkipTLSVerify bool `yaml:"insecure-skip-tls-verify"`
-		} `yaml:"user"`
-	} `yaml:"users"`
-}
-
-func PrepareKubeconf(configPath string, serverPort uint16) error {
-	data, err := os.ReadFile(configPath)
-	if err != nil {
-		return err
-	}
-
-	var kubeconfig KubeConfig
-	err = yaml.Unmarshal(data, &kubeconfig)
-	if err != nil {
-		return err
-	}
-
-	kubeconfig.Clusters[0].Cluster.Server = "https://127.0.0.1:" + fmt.Sprintf("%d", serverPort)
-	kubeconfig.Clusters[0].Cluster.TLSVerify = true
-
-	updatedData, err := yaml.Marshal(&kubeconfig)
-	if err != nil {
-		log.Fatalf("Error marshalling kubeconfig: %v", err)
-	}
-
-	err = os.WriteFile(configPath, updatedData, 0644)
-	if err != nil {
-		log.Fatalf("Error writing updated kubeconfig file: %v", err)
-	}
-
-	fmt.Println("Kubeconfig file updated successfully.")
-
-	return nil
-}
-
-func k8sApply(manifestPath string) error {
-	kubeconfig := filepath.Join(
-		".kube", "config",
-	)
-	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
-	if err != nil {
-		log.Fatalf("Error building kubeconfig: %v", err)
-	}
-
+func k8sApply(config *rest.Config, manifestPath string) error {
 	dynamicClient, err := dynamic.NewForConfig(config)
 	if err != nil {
 		log.Fatalf("Error creating dynamic client: %v", err)
