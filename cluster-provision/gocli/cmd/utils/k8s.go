f4565a770ad5b44b9f1caa8f1b3de9296401b47e
aerosouund <aerosound161@gmail.com>
2024-05-19 13:43:16 +0300

    register crd

--- cluster-provision/gocli/cmd/utils/k8s.go
+++ cluster-provision/gocli/cmd/utils/k8s.go
@@ -7,7 +7,6 @@ import (
 	"os"
 
 	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
-	crdclient "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
 	"k8s.io/apimachinery/pkg/api/meta"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
@@ -19,33 +18,6 @@ import (
 	"k8s.io/client-go/rest"
 	"sigs.k8s.io/yaml"
 )
-
-func CreateCRD(config *rest.Config, manifestPath string) error {
-	yamlData, err := os.ReadFile(manifestPath)
-	if err != nil {
-		return fmt.Errorf("Error reading YAML file: %v", err)
-	}
-
-	jsonData, err := yaml.YAMLToJSON(yamlData)
-	if err != nil {
-		return fmt.Errorf("Error converting YAML to JSON: %v", err)
-	}
-
-	crd := &apiextensionsv1.CustomResourceDefinition{}
-	dec := serializer.NewCodecFactory(scheme.Scheme).UniversalDeserializer()
-	_, _, err = dec.Decode(jsonData, nil, crd)
-	if err != nil {
-		return fmt.Errorf("Error decoding JSON to Unstructured object: %v", err)
-	}
-
-	crdClientSet, err := crdclient.NewForConfig(config)
-	if err != nil {
-		return err
-	}
-	_, err = crdClientSet.ApiextensionsV1().CustomResourceDefinitions().Create(context.Background(), crd, v1.CreateOptions{})
-	fmt.Printf("CRD %v applied successfully!\n", manifestPath)
-	return nil
-}
 
 func K8sApply(config *rest.Config, manifestPath string) error {
 	dynamicClient, err := dynamic.NewForConfig(config)
@@ -62,6 +34,7 @@ func K8sApply(config *rest.Config, manifestPath string) error {
 	yamlDocs := bytes.Split(yamlData, []byte("---\n"))
 	s := runtime.NewScheme()
 	scheme.AddToScheme(s)
+	apiextensionsv1.AddToScheme(s)
 
 	for i, yamlDoc := range yamlDocs {
 		if len(yamlDoc) == 0 {
@@ -79,7 +52,8 @@ func K8sApply(config *rest.Config, manifestPath string) error {
 		dec := serializer.NewCodecFactory(s).UniversalDeserializer()
 		_, _, err = dec.Decode(jsonData, nil, obj)
 		if err != nil {
-			return fmt.Errorf("Error decoding JSON to Unstructured object: %v", err)
+			fmt.Printf("Error decoding JSON to Unstructured object: %v", err)
+			continue
 		}
 
 		gvk := obj.GroupVersionKind()
