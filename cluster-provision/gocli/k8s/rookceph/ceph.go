d8398ad73b015585ba6339899cef758d5980c7ba
aerosouund <aerosound161@gmail.com>
2024-05-21 13:34:18 +0300

    all ceph manifests

--- cluster-provision/gocli/k8s/rookceph/ceph.go
+++ cluster-provision/gocli/k8s/rookceph/ceph.go
@@ -21,6 +21,11 @@ func (o *CephOpt) Exec() error {
 		"/workdir/manifests/ceph/snapshot.storage.k8s.io_volumesnapshotclasses.yaml",
 		"/workdir/manifests/ceph/rbac-snapshot-controller.yaml",
 		"/workdir/manifests/ceph/setup-snapshot-controller.yaml",
+		"/workdir/manifests/ceph/common.yaml",
+		"/workdir/manifests/ceph/crds.yaml",
+		"/workdir/manifests/ceph/operator.yaml",
+		"/workdir/manifests/ceph/cluster-test.yaml",
+		"/workdir/manifests/ceph/pool-test.yaml",
 	}
 
 	for _, manifest := range manifests {
@@ -29,5 +34,17 @@ func (o *CephOpt) Exec() error {
 			return err
 		}
 	}
+	// blockpools, err := o.client.List(schema.GroupVersionKind{
+	// 	Group:   "ceph.rook.io",
+	// 	Version: "v1",
+	// 	Kind:    "cephblockpools"},
+	// 	"rook-ceph")
+	// for _, bp := range blockpools.Items {
+	// 	fmt.Println("found block pool", bp.GetName())
+	// }
+
+	// if err != nil {
+	// 	return err
+	// }
 	return nil
 }
