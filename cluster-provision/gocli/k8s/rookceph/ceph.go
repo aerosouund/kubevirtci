7edbbaf18a380ec30fe715616e7fdacc935033cd
aerosouund <aerosound161@gmail.com>
2024-05-21 15:57:45 +0300

    nil safeguard

--- cluster-provision/gocli/k8s/rookceph/ceph.go
+++ cluster-provision/gocli/k8s/rookceph/ceph.go
@@ -63,8 +63,7 @@ func (o *CephOpt) Exec() error {
 		time.Sleep(10 * time.Second)
 	}
 
-	if blockpool.Status.Phase != "Ready" {
-		fmt.Println("block pool has status", blockpool.Status.Phase)
+	if blockpool.Status != nil && blockpool.Status.Phase != "Ready" {
 		return fmt.Errorf("CephBlockPool replica pool did not become ready after %d retries", maxRetries)
 	}
 
