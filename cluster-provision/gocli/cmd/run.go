01a401174f005884774f410b053fd1f6e3e04c1b
aerosouund <aerosound161@gmail.com>
2024-05-26 11:45:40 +0300

    exec bash

--- cluster-provision/gocli/cmd/run.go
+++ cluster-provision/gocli/cmd/run.go
@@ -727,22 +727,22 @@ func run(cmd *cobra.Command, args []string) (retErr error) {
 			wg.Done()
 		}(node.ID)
 	}
-	err = utils.Compile("node01")
+	// err = utils.Compile("node01")
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	err = jumpSCP(workerSSHPort, 1, "/workdir/scripts/node01.sh")
 	if err != nil {
 		panic(err)
 	}
 
-	err = jumpSCP(workerSSHPort, 1, "/workdir/bin/node01")
-	if err != nil {
-		panic(err)
-	}
+	// _, err = jumpSSH(workerSSHPort, 1, "sudo chmod +x node01", false)
+	// if err != nil {
+	// 	panic(err)
+	// }
 
-	_, err = jumpSSH(workerSSHPort, 1, "sudo chmod +x node01", false)
-	if err != nil {
-		panic(err)
-	}
-
-	_, err = jumpSSH(workerSSHPort, 1, "sudo ./node01", true)
+	_, err = jumpSSH(workerSSHPort, 1, "sudo /bin/bash -c node01.sh", true)
 	if err != nil {
 		panic(err)
 	}
