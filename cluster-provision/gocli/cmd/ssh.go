d10255925c446224b4d147686144687a0cad0d01
aerosouund <aerosound161@gmail.com>
2024-05-26 11:28:15 +0300

    check error

--- cluster-provision/gocli/cmd/ssh.go
+++ cluster-provision/gocli/cmd/ssh.go
@@ -112,7 +112,7 @@ func jumpSSH(sshPort uint16, nodeIdx int, cmd string) (string, error) {
 
 	err = session.Run(cmd)
 	if err != nil {
-		return "", fmt.Errorf("Failed to execute command: %v", err)
+		return "", fmt.Errorf("Failed to execute command: %v, %v", err, stderr.String())
 	}
 	return stdout.String(), nil
 }
