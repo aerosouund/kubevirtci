6d2ac7f175bf1a91c6ca4226c0e0f5b9e8fccfec
aerosouund <aerosound161@gmail.com>
2024-06-01 15:47:24 +0300

    sudo

--- cluster-provision/gocli/opts/node01/node01.go
+++ cluster-provision/gocli/opts/node01/node01.go
@@ -31,7 +31,7 @@ func (n *Node01Provisioner) Exec() error {
 		`sudo kubectl --kubeconfig=/etc/kubernetes/admin.conf taint nodes node01 node-role.kubernetes.io/control-plane:NoSchedule-`,
 		`sudo kubectl --kubeconfig=/etc/kubernetes/admin.conf get nodes --no-headers; kubectl_rc=$?; retry_counter=0; while [[ $retry_counter -lt 20 && $kubectl_rc -ne 0 ]]; do sleep 10; echo "Waiting for api server to be available..."; sudo kubectl --kubeconfig=/etc/kubernetes/admin.conf get nodes --no-headers; kubectl_rc=$?; retry_counter=$((retry_counter + 1)); done`,
 		"sudo kubectl --kubeconfig=/etc/kubernetes/admin.conf version",
-		`kubectl --kubeconfig=/etc/kubernetes/admin.conf create -f /provision/local-volume.yaml`,
+		`sudo kubectl --kubeconfig=/etc/kubernetes/admin.conf create -f /provision/local-volume.yaml`,
 		"mkdir -p /var/lib/rook",
 		"chcon -t container_file_t /var/lib/rook",
 	}
