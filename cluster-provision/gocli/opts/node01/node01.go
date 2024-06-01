84f720c1b2314e0f2004cd7c2ab535a98da27f85
aerosouund <aerosound161@gmail.com>
2024-06-01 15:24:44 +0300

    export

--- cluster-provision/gocli/opts/node01/node01.go
+++ cluster-provision/gocli/opts/node01/node01.go
@@ -18,7 +18,7 @@ func NewNode01Provisioner(sshPort uint16) *Node01Provisioner {
 
 func (n *Node01Provisioner) Exec() error {
 	cmds := []string{
-		`[ -f /home/vagrant/single_stack ] && kubeadm_conf="/etc/kubernetes/kubeadm_ipv6.conf" && cni_manifest="/provision/cni_ipv6.yaml" || { kubeadm_conf="/etc/kubernetes/kubeadm.conf"; cni_manifest="/provision/cni.yaml"; }`,
+		`[ -f /home/vagrant/single_stack ] && export kubeadm_conf="/etc/kubernetes/kubeadm_ipv6.conf" && export cni_manifest="/provision/cni_ipv6.yaml" || { export kubeadm_conf="/etc/kubernetes/kubeadm.conf"; export cni_manifest="/provision/cni.yaml"; }`,
 		// `[ -f /home/vagrant/enable_audit ] && apiVer=$(head -1 /etc/kubernetes/audit/adv-audit.yaml) && echo "$apiVer" > /etc/kubernetes/audit/adv-audit.yaml && echo -e "kind: Policy\nrules:\n- level: Metadata" >> /etc/kubernetes/audit/adv-audit.yaml`,
 		`timeout=30; interval=5; while ! hostnamectl | grep Transient; do echo "Waiting for dhclient to set the hostname from dnsmasq"; sleep $interval; timeout=$((timeout - interval)); [ $timeout -le 0 ] && exit 1; done`,
 		// `[ -f /sys/fs/cgroup/cgroup.controllers ] && echo "Configuring cgroup v2" && CRIO_CONF_DIR=/etc/crio/crio.conf.d && mkdir -p ${CRIO_CONF_DIR} && sudo echo '[crio.runtime]\nconmon_cgroup = "pod"\ncgroup_manager = "cgroupfs"' > ${CRIO_CONF_DIR}/00-cgroupv2.conf && sudo sed -i 's/--cgroup-driver=systemd/--cgroup-driver=cgroupfs/' /etc/sysconfig/kubelet && sudo systemctl stop kubelet && sudo systemctl restart crio && sudo systemctl start kubelet`,
@@ -29,7 +29,7 @@ func (n *Node01Provisioner) Exec() error {
 		`sudo kubectl --kubeconfig=/etc/kubernetes/admin.conf patch deployment coredns -n kube-system -p "$(cat /provision/kubeadm-patches/add-security-context-deployment-patch.yaml)"`, // todo: dont make this depend on the node container
 		`sudo kubectl --kubeconfig=/etc/kubernetes/admin.conf create -f "$cni_manifest"`,
 		`sudo kubectl --kubeconfig=/etc/kubernetes/admin.conf taint nodes node01 node-role.kubernetes.io/control-plane:NoSchedule-`,
-		`sudo kubectl --kubeconfig=/etc/kubernetes/admin.conf get nodes --no-headers; kubectl_rc=$?; retry_counter=0; while [[ $retry_counter -lt 20 && $kubectl_rc -ne 0 ]]; do sleep 10; echo "Waiting for api server to be available..."; kubectl --kubeconfig=/etc/kubernetes/admin.conf get nodes --no-headers; kubectl_rc=$?; retry_counter=$((retry_counter + 1)); done`,
+		`sudo kubectl --kubeconfig=/etc/kubernetes/admin.conf get nodes --no-headers; kubectl_rc=$?; retry_counter=0; while [[ $retry_counter -lt 20 && $kubectl_rc -ne 0 ]]; do sleep 10; echo "Waiting for api server to be available..."; sudo kubectl --kubeconfig=/etc/kubernetes/admin.conf get nodes --no-headers; kubectl_rc=$?; retry_counter=$((retry_counter + 1)); done`,
 		"sudo kubectl --kubeconfig=/etc/kubernetes/admin.conf version",
 		`local_volume_manifest="/provision/local-volume.yaml"; kubectl --kubeconfig=/etc/kubernetes/admin.conf create -f "$local_volume_manifest"`,
 		"mkdir -p /var/lib/rook",
