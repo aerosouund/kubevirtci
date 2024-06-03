8778ecc697b41581a41c396bd63a81c5e0898556
aerosouund <aerosound161@gmail.com>
2024-06-03 17:14:49 +0300

    thats just fucking stupid

--- cluster-provision/gocli/opts/nodes/nodes.go
+++ cluster-provision/gocli/opts/nodes/nodes.go
@@ -24,23 +24,21 @@ func NewNodesProvisioner(sshPort uint16, nodeIdx int) *NodesProvisioner {
 }
 
 func (n *NodesProvisioner) Exec() error {
-	// cgroupv2, err := f.ReadFile("conf/00-cgroupv2.conf")
-	// if err != nil {
-	// 	return err
-	// }
+	cgroupv2, err := f.ReadFile("conf/00-cgroupv2.conf")
+	if err != nil {
+		return err
+	}
 
 	cmds := []string{
-		// "echo 'entering sleep' && sleep 9000000",
-		// "source /var/lib/kubevirtci/shared_vars.sh",
-		// `timeout=30; interval=5; while ! hostnamectl | grep Transient; do echo "Waiting for dhclient to set the hostname from dnsmasq"; sleep $interval; timeout=$((timeout - interval)); [ $timeout -le 0 ] && exit 1; done`,
-		// `[ -f /sys/fs/cgroup/cgroup.controllers ] && mkdir -p /etc/crio/crio.conf.d && echo '` + string(cgroupv2) + `' | sudo tee /etc/crio/crio.conf.d/00-cgroupv2.conf > /dev/null && sudo sed -i 's/--cgroup-driver=systemd/--cgroup-driver=cgroupfs/' /etc/sysconfig/kubelet && sudo systemctl stop kubelet && sudo systemctl restart crio`,
-		// "while [[ $(systemctl status crio | grep -c active) -eq 0 ]]; do sleep 2; done",
-		// `echo "KUBELET_EXTRA_ARGS=${KUBELET_CGROUP_ARGS} --fail-swap-on=false ${nodeip} --feature-gates=CPUManager=true,NodeSwap=true --cpu-manager-policy=static --kube-reserved=cpu=250m --system-reserved=cpu=250m" | sudo tee /etc/sysconfig/kubelet > /dev/null`, // todo: add the condition
-		// "sudo systemctl daemon-reload && sudo service kubelet restart",
-		// "kubelet_rc=$?; [[ $kubelet_rc -ne 0 ]] && rm -rf /var/lib/kubelet/cpu_manager_state && service kubelet restart",
-		// "sudo swapoff -a",
-		// "until ip address show dev eth0 | grep global | grep inet6; do sleep 1; done",
-		"sleep 10",
+		"source /var/lib/kubevirtci/shared_vars.sh",
+		`timeout=30; interval=5; while ! hostnamectl | grep Transient; do echo "Waiting for dhclient to set the hostname from dnsmasq"; sleep $interval; timeout=$((timeout - interval)); [ $timeout -le 0 ] && exit 1; done`,
+		`[ -f /sys/fs/cgroup/cgroup.controllers ] && mkdir -p /etc/crio/crio.conf.d && echo '` + string(cgroupv2) + `' | sudo tee /etc/crio/crio.conf.d/00-cgroupv2.conf > /dev/null && sudo sed -i 's/--cgroup-driver=systemd/--cgroup-driver=cgroupfs/' /etc/sysconfig/kubelet && sudo systemctl stop kubelet && sudo systemctl restart crio`,
+		"while [[ $(systemctl status crio | grep -c active) -eq 0 ]]; do sleep 2; done",
+		`echo "KUBELET_EXTRA_ARGS=${KUBELET_CGROUP_ARGS} --fail-swap-on=false ${nodeip} --feature-gates=CPUManager=true,NodeSwap=true --cpu-manager-policy=static --kube-reserved=cpu=250m --system-reserved=cpu=250m" | sudo tee /etc/sysconfig/kubelet > /dev/null`, // todo: add the condition
+		"sudo systemctl daemon-reload && sudo service kubelet restart",
+		"kubelet_rc=$?; [[ $kubelet_rc -ne 0 ]] && rm -rf /var/lib/kubelet/cpu_manager_state && service kubelet restart",
+		"sudo swapoff -a",
+		"until ip address show dev eth0 | grep global | grep inet6; do sleep 1; done",
 		"sudo kubeadm join --token abcdef.1234567890123456 192.168.66.101:6443 --ignore-preflight-errors=all --discovery-token-unsafe-skip-ca-verification=true",
 		"sudo mkdir -p /var/lib/rook",
 		"sudo chcon -t container_file_t /var/lib/rook",
@@ -48,7 +46,7 @@ func (n *NodesProvisioner) Exec() error {
 
 	for _, cmd := range cmds {
 		logrus.Info("executing: ", cmd)
-		_, err := utils.JumpSSH(n.sshPort, 1, cmd, true)
+		_, err := utils.JumpSSH(n.sshPort, n.nodeIdx, cmd, true)
 		if err != nil {
 			return fmt.Errorf("error executing %s: %s", cmd, err)
 		}
