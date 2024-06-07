b368d2e8d12c841e72c649e236bf0652c65f082c
aerosouund <aerosound161@gmail.com>
2024-06-07 15:32:03 +0300

    try to see object

--- cluster-provision/gocli/opts/rookceph/ceph_test.go
+++ cluster-provision/gocli/opts/rookceph/ceph_test.go
@@ -4,12 +4,29 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	k8s "kubevirt.io/kubevirtci/cluster-provision/gocli/utils/k8s"
+	"go.uber.org/mock/gomock"
+	kubevirtcimocks "kubevirt.io/kubevirtci/cluster-provision/gocli/utils/mock"
 )
 
 func TestCephOpt(t *testing.T) {
-	client := k8s.NewTestClient()
-	opt := NewCephOpt(client)
+	mockK8sClient := kubevirtcimocks.NewMockK8sDynamicClient(gomock.NewController(t))
+
+	// obj := unstructured.Unstructured{
+	// 	Object: make(map[string]interface{}),
+	// }
+
+	opt := NewCephOpt(mockK8sClient)
+	mockK8sClient.EXPECT().Apply(gomock.Any(), "manifests/snapshot.storage.k8s.io_volumesnapshots.yaml").Return(nil)
+	mockK8sClient.EXPECT().Apply(gomock.Any(), "manifests/snapshot.storage.k8s.io_volumesnapshotcontents.yaml").Return(nil)
+	mockK8sClient.EXPECT().Apply(gomock.Any(), "manifests/snapshot.storage.k8s.io_volumesnapshotclasses.yaml").Return(nil)
+	mockK8sClient.EXPECT().Apply(gomock.Any(), "manifests/rbac-snapshot-controller.yaml").Return(nil)
+	mockK8sClient.EXPECT().Apply(gomock.Any(), "manifests/setup-snapshot-controller.yaml").Return(nil)
+	mockK8sClient.EXPECT().Apply(gomock.Any(), "manifests/common.yaml").Return(nil)
+	mockK8sClient.EXPECT().Apply(gomock.Any(), "manifests/crds.yaml").Return(nil)
+	mockK8sClient.EXPECT().Apply(gomock.Any(), "manifests/operator.yaml").Return(nil)
+	mockK8sClient.EXPECT().Apply(gomock.Any(), "manifests/cluster-test.yaml").Return(nil)
+	mockK8sClient.EXPECT().Apply(gomock.Any(), "manifests/pool-test.yaml").Return(nil)
+
 	err := opt.Exec()
 	assert.NoError(t, err)
 }
