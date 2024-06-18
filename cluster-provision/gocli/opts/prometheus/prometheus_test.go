eb43693f33d23d0cf055cf2283de2dc54d9a0766
aerosouund <aerosound161@gmail.com>
2024-06-18 16:15:43 +0300

    supply objects to the test client

--- cluster-provision/gocli/opts/prometheus/prometheus_test.go
+++ cluster-provision/gocli/opts/prometheus/prometheus_test.go
@@ -4,11 +4,12 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"k8s.io/apimachinery/pkg/runtime"
 	k8s "kubevirt.io/kubevirtci/cluster-provision/gocli/utils/k8s"
 )
 
 func TestPrometheusOpt(t *testing.T) {
-	client := k8s.NewTestClient()
+	client := k8s.NewTestClient([]runtime.Object{})
 	opt := NewPrometheusOpt(client, true, true)
 	err := opt.Exec()
 	assert.NoError(t, err)
