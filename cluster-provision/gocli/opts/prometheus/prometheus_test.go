7dc16e6871c920a0eb88290645800dc57d9d7e08
aerosouund <aerosound161@gmail.com>
2024-06-18 16:54:13 +0300

    use reactor

--- cluster-provision/gocli/opts/prometheus/prometheus_test.go
+++ cluster-provision/gocli/opts/prometheus/prometheus_test.go
@@ -4,12 +4,11 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"k8s.io/apimachinery/pkg/runtime"
 	k8s "kubevirt.io/kubevirtci/cluster-provision/gocli/utils/k8s"
 )
 
 func TestPrometheusOpt(t *testing.T) {
-	client := k8s.NewTestClient([]runtime.Object{})
+	client := k8s.NewTestClient()
 	opt := NewPrometheusOpt(client, true, true)
 	err := opt.Exec()
 	assert.NoError(t, err)
--- cluster-provision/gocli/opts/rookceph/ceph_test.go
+++ cluster-provision/gocli/opts/rookceph/ceph_test.go
@@ -8,27 +8,28 @@ import (
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	k8stesting "k8s.io/client-go/testing"
 	k8s "kubevirt.io/kubevirtci/cluster-provision/gocli/utils/k8s"
 	kubevirtcimocks "kubevirt.io/kubevirtci/cluster-provision/gocli/utils/mock"
 )
 
 func TestWithFakeClient(t *testing.T) {
-	obj := &unstructured.Unstructured{
-		Object: map[string]interface{}{
-			"apiVersion": "ceph.rook.io/v1",
-			"kind":       "CephBlockPool",
-			"metadata": map[string]interface{}{
-				"name":      "replicapool",
-				"namespace": "rook-ceph",
-			},
-			"status": map[string]interface{}{
-				"phase": "Ready",
-			},
-			"spec": map[string]interface{}{},
-		},
+	updateBlockPool := func(action k8stesting.Action) (bool, runtime.Object, error) {
+		createAction := action.(k8stesting.CreateAction)
+		obj := createAction.GetObject().(*unstructured.Unstructured)
+
+		status := map[string]interface{}{
+			"phase": "Ready",
+		}
+		if err := unstructured.SetNestedField(obj.Object, status, "status"); err != nil {
+			return true, nil, err
+		}
+
+		return false, obj, nil
 	}
 
-	testClient := k8s.NewTestClient([]runtime.Object{obj})
+	r := k8s.NewReactorConfig("create", "cephblockpools", updateBlockPool)
+	testClient := k8s.NewTestClient(r)
 	opt := NewCephOpt(testClient)
 	err := opt.Exec()
 	assert.NoError(t, err)
