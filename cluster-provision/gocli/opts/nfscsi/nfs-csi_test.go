394b83fe61c926eac7623190e2a4ae1c3ad323bd
aerosouund <aerosound161@gmail.com>
2024-06-18 16:54:13 +0300

    use reactor

--- cluster-provision/gocli/opts/nfscsi/nfs-csi_test.go
+++ cluster-provision/gocli/opts/nfscsi/nfs-csi_test.go
@@ -4,12 +4,12 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"k8s.io/apimachinery/pkg/runtime"
 	k8s "kubevirt.io/kubevirtci/cluster-provision/gocli/utils/k8s"
 )
 
 func TestNfsCsiOpt(t *testing.T) {
-	client := k8s.NewTestClient([]runtime.Object{})
+
+	client := k8s.NewTestClient()
 	opt := NewNfsCsiOpt(client)
 	err := opt.Exec()
 	assert.NoError(t, err)
