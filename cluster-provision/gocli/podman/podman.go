48c68bbc5ebcc8b11c4ddc921d50be3c3dada26b
aerosouund <aerosound161@gmail.com>
2024-07-03 15:15:37 +0300

    podman package

--- cluster-provision/gocli/podman/podman.go
+++ cluster-provision/gocli/podman/podman.go
@@ -2,11 +2,11 @@ package podman
 
 import (
 	"context"
-	"fmt"
-	"os"
 
+	"github.com/containers/podman/v5/libpod/define"
 	"github.com/containers/podman/v5/pkg/bindings"
 	"github.com/containers/podman/v5/pkg/bindings/containers"
+	"github.com/containers/podman/v5/pkg/bindings/images"
 	"github.com/containers/podman/v5/pkg/specgen"
 )
 
@@ -24,19 +24,43 @@ func NewPodman() (*Podman, error) {
 	}, nil
 }
 
-func (p *Podman) Run(image string) (string, error) {
-	s := specgen.NewSpecGenerator("quay.io/libpod/alpine_nginx", false)
-	s.Name = "foobar"
+func (p *Podman) ImagePull(image string) error {
+	if _, err := images.Pull(p.Conn, image, nil); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (p *Podman) Create(image string) (string, error) {
+	s := specgen.NewSpecGenerator(image, false)
 	createResponse, err := containers.CreateWithSpec(p.Conn, s, nil)
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		return "", err
 	}
-	fmt.Println("Container created.")
-	if err := containers.Start(p.Conn, createResponse.ID, nil); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+	return createResponse.ID, nil
+}
+
+func (p *Podman) Run(containerID string) error {
+	if err := containers.Start(p.Conn, containerID, nil); err != nil {
+		return err
 	}
-	fmt.Println("Container started.")
-	return "", nil
+	return nil
+}
+
+func (p *Podman) Inspect(containerID string) (*define.InspectContainerData, error) {
+	inspectData, err := containers.Inspect(p.Conn, "foobar", new(containers.InspectOptions).WithSize(true))
+	if err != nil {
+		return nil, err
+	}
+	// Print the container ID
+	return inspectData, nil
+
+}
+
+func (p *Podman) Remove(containerID string) error {
+	if _, err := containers.Remove(p.Conn, containerID, nil); err != nil {
+		return err
+	}
+	return nil
 }
