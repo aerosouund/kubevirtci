fb7b3d472a6beebf5595d3f0a5e0e22c72690f90
aerosouund <aerosound161@gmail.com>
2024-07-06 14:50:44 +0300

    my bad

--- cluster-provision/gocli/cri/docker/docker.go
+++ cluster-provision/gocli/cri/docker/docker.go
@@ -91,7 +91,7 @@ func (dc *DockerClient) Create(image string, createOpts *cri.CreateOpts) (string
 		fmt.Println(err)
 	}
 	logrus.Info("created registry container with id: ", string(containerID))
-	return strings.TrimPrefix(string(containerID), "\n"), nil
+	return strings.TrimSuffix(string(containerID), "\n"), nil
 }
 
 func (dc *DockerClient) Remove(containerID string) error {
