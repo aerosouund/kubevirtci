29e4ec330caf10850cea893cd2dcd75b20d4204f
aerosouund <aerosound161@gmail.com>
2024-07-12 14:59:41 +0300

    centos

--- cluster-provision/gocli/utils/ssh/ssh.go
+++ cluster-provision/gocli/utils/ssh/ssh.go
@@ -31,7 +31,7 @@ func NewSSHClient(port uint16, idx int, root bool) (*SSHClientImpl, error) {
 	if err != nil {
 		return nil, err
 	}
-	u := "bullshit"
+	u := "centos"
 	if root {
 		u = "root"
 	}
