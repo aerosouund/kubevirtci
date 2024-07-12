ae6637f32d28940d890c32f736b2805b65d643d5
aerosouund <aerosound161@gmail.com>
2024-07-12 18:15:51 +0300

    tmkdir

--- cluster-provision/gocli/opts/provision/provision.go
+++ cluster-provision/gocli/opts/provision/provision.go
@@ -27,7 +27,8 @@ func (l *LinuxProvisioner) Exec() error {
 	}
 
 	cmds := []string{
-		"touch /var/lib/kubevirtci/shared_vars.sh",
+
+		"mkdir /var/lib/kubevirtci && touch /var/lib/kubevirtci/shared_vars.sh",
 		`echo '` + string(sharedVars) + `' |  tee /var/lib/kubevirtci/shared_vars.sh > /dev/null`,
 		`dnf install -y "kernel-modules-$(uname -r)"`,
 		"dnf install -y cloud-utils-growpart",
