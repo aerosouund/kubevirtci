23972285fa0004bc51a4bae5cb10f5448a7ee042
aerosouund <aerosound161@gmail.com>
2024-07-15 15:55:40 +0300

    regex

--- cluster-provision/gocli/opts/k8sprovision/k8sprovision.go
+++ cluster-provision/gocli/opts/k8sprovision/k8sprovision.go
@@ -31,23 +31,6 @@ func NewK8sProvisioner(sshClient utils.SSHClient, version string) *K8sProvisione
 }
 
 func (k *K8sProvisioner) Exec() error {
-	_, err := k.sshClient.SSH(`image_regex='([a-z0-9\_\.]+[/-]?)+(@sha256)?:[a-z0-9\_\.\-]+' image_regex_w_double_quotes='"?'"${image_regex}"'"?' find /tmp -type f -name '*.yaml' -print0 | xargs -0 grep -iE '(image|value): '"${image_regex_w_double_quotes}" > /tmp/test`, true)
-	if err != nil {
-		return err
-	}
-
-	images, err := k.sshClient.SSH(`image_regex='([a-z0-9\_\.]+[/-]?)+(@sha256)?:[a-z0-9\_\.\-]+' && image_regex_w_double_quotes='"?'"${image_regex}"'"?' && grep -ioE "${image_regex_w_double_quotes}" /tmp/test`, false)
-	if err != nil {
-		return err
-	}
-
-	imagesList := strings.Split(images, "\n")
-	for _, image := range imagesList {
-		err := k.pullImageRetry(image)
-		if err != nil {
-			logrus.Infof("Failed to pull image: %s, it will not be available offline", image)
-		}
-	}
 
 	crio, err := f.ReadFile("conf/crio-yum.repo")
 	if err != nil {
@@ -172,6 +155,24 @@ func (k *K8sProvisioner) Exec() error {
 	extraImg, err := f.ReadFile("conf/extra-images")
 	if err != nil {
 		return err
+	}
+
+	_, err = k.sshClient.SSH(`image_regex='([a-z0-9\_\.]+[/-]?)+(@sha256)?:[a-z0-9\_\.\-]+' image_regex_w_double_quotes='"?'"${image_regex}"'"?' find /tmp -type f -name '*.yaml' -print0 | xargs -0 grep -iE '(image|value): '"${image_regex_w_double_quotes}" > /tmp/test`, true)
+	if err != nil {
+		return err
+	}
+
+	images, err := k.sshClient.SSH(`image_regex='([a-z0-9\_\.]+[/-]?)+(@sha256)?:[a-z0-9\_\.\-]+' && image_regex_w_double_quotes='"?'"${image_regex}"'"?' && grep -ioE "${image_regex_w_double_quotes}" /tmp/test`, false)
+	if err != nil {
+		return err
+	}
+
+	imagesList := strings.Split(images, "\n")
+	for _, image := range imagesList {
+		err := k.pullImageRetry(image)
+		if err != nil {
+			logrus.Infof("Failed to pull image: %s, it will not be available offline", image)
+		}
 	}
 
 	imagesList = strings.Split(string(extraImg), "\n")
