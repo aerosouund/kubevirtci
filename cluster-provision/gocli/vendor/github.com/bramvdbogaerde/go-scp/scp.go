c38a4654b546b7664fa0053cd751a298fd2fc22f
aerosouund <aerosound161@gmail.com>
2024-07-27 12:44:55 +0300

    ssh bug

--- cluster-provision/gocli/vendor/github.com/bramvdbogaerde/go-scp/scp.go
+++ cluster-provision/gocli/vendor/github.com/bramvdbogaerde/go-scp/scp.go
@@ -4,8 +4,7 @@
  * along with the source code.
  */
 
-// Package scp.
-// Simple scp package to copy files over SSH.
+// Simple scp package to copy files over SSH
 package scp
 
 import (
@@ -14,24 +13,33 @@ import (
 	"golang.org/x/crypto/ssh"
 )
 
-// NewClient returns a new scp.Client with provided host and ssh.clientConfig.
+// Returns a new scp.Client with provided host and ssh.clientConfig
+// It has a default timeout of one minute.
 func NewClient(host string, config *ssh.ClientConfig) Client {
 	return NewConfigurer(host, config).Create()
 }
 
-// NewClientWithTimeout returns a new scp.Client with provides host, ssh.ClientConfig and timeout.
+// Returns a new scp.Client with provides host, ssh.ClientConfig and timeout
 // Deprecated: provide meaningful context to each "Copy*" function instead.
 func NewClientWithTimeout(host string, config *ssh.ClientConfig, timeout time.Duration) Client {
 	return NewConfigurer(host, config).Timeout(timeout).Create()
 }
 
-// NewClientBySSH returns a new scp.Client using an already existing established SSH connection.
+// Returns a new scp.Client using an already existing established SSH connection
 func NewClientBySSH(ssh *ssh.Client) (Client, error) {
-	return NewConfigurer("", nil).SSHClient(ssh).Create(), nil
+	session, err := ssh.NewSession()
+	if err != nil {
+		return Client{}, err
+	}
+	return NewConfigurer("", nil).Session(session).Create(), nil
 }
 
-// NewClientBySSHWithTimeout same as NewClientWithTimeout but uses an existing SSH client.
+// Same as NewClientWithTimeout but uses an existing SSH client
 // Deprecated: provide meaningful context to each "Copy*" function instead.
 func NewClientBySSHWithTimeout(ssh *ssh.Client, timeout time.Duration) (Client, error) {
-	return NewConfigurer("", nil).SSHClient(ssh).Timeout(timeout).Create(), nil
+	session, err := ssh.NewSession()
+	if err != nil {
+		return Client{}, err
+	}
+	return NewConfigurer("", nil).Session(session).Timeout(timeout).Create(), nil
 }
