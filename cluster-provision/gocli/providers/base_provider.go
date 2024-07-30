f54af883767019349dc1d989e786608089dbf03a
aerosouund <aerosound161@gmail.com>
2024-07-30 21:05:26 +0300

    vmlinux

--- cluster-provision/gocli/providers/base_provider.go
+++ cluster-provision/gocli/providers/base_provider.go
@@ -174,7 +174,7 @@ func (kp *KubevirtProvider) Provision(ctx context.Context, cancel context.Cancel
 			"/var/run/disk":     {},
 			"/var/lib/registry": {},
 		},
-		Cmd: []string{"/bin/bash", "-c", fmt.Sprintf("sleep 90000000")},
+		Cmd: []string{"/bin/bash", "-c", fmt.Sprintf("/vm.sh --memory %s --cpu %s %s", kp.Memory, strconv.Itoa(int(kp.CPU)), kp.QemuArgs)},
 	}, &container.HostConfig{
 		Mounts: []mount.Mount{
 			{
