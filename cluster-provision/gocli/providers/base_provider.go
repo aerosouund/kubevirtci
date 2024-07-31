3a82228655f92db8ceea9d94d6bab15e899d29fc
aerosouund <aerosound161@gmail.com>
2024-07-31 16:29:00 +0300

    sleep

--- cluster-provision/gocli/providers/base_provider.go
+++ cluster-provision/gocli/providers/base_provider.go
@@ -210,6 +210,8 @@ func (kp *KubevirtProvider) Provision(ctx context.Context, cancel context.Cancel
 	if err != nil {
 		return err
 	}
+
+	time.Sleep(time.Minute * 10)
 	rootkey := rootkey.NewRootKey(sshClient)
 	if err = rootkey.Exec(); err != nil {
 		return err
