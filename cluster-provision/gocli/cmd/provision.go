ed66e73e8c8a211d57b7a64c22ead2dc7ea8c3b7
aerosouund <aerosound161@gmail.com>
2024-08-13 12:14:48 +0300

    vcontains

--- cluster-provision/gocli/cmd/provision.go
+++ cluster-provision/gocli/cmd/provision.go
@@ -16,7 +16,7 @@ var versionMap = map[string]string{
 	"1.30": "1.30.2",
 	"1.29": "1.29.6",
 	"1.28": "1.28.11",
-	"1.31": "1.31.0-beta.0",
+	"1.31": "1.31.0",
 }
 
 const base = "quay.io/kubevirtci/centos9-base"
