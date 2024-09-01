24dd8e46485d181a143822d79d9f27449c9cb075
aerosouund <aerosound161@gmail.com>
2024-09-01 14:56:49 +0300

    echo files

--- cluster-provision/gocli/opts/istio/istio.go
+++ cluster-provision/gocli/opts/istio/istio.go
@@ -64,6 +64,8 @@ func (o *istioOperatorOpt) Exec() error {
 	cmds := []string{
 		"source /var/lib/kubevirtci/shared_vars.sh",
 		"PATH=/opt/istio-" + istioVersion + "/bin:$PATH istioctl --kubeconfig /etc/kubernetes/admin.conf --hub quay.io/kubevirtci operator init",
+		"echo '" + string(istioNoCnao) + "' > /opt/istio/istio-operator.cr.yaml",
+		"echo '" + string(istioWithCnao) + "' > /opt/istio/istio-operator.cr.yaml",
 	}
 	for _, cmd := range cmds {
 		if err := o.sshClient.Command(cmd); err != nil {
